12d0a2b9dd906c309bb6f78c501715aaec345018
Cihat <44507004+yigitcihat@users.noreply.github.com>
2021-02-02 18:23:40 +0300

    Draw Panel 2D drawing is better now

--- Assets/[Game] - Rename This/Scripts/SwipeControl.cs
+++ Assets/[Game] - Rename This/Scripts/SwipeControl.cs
@@ -33,7 +33,6 @@ public class SwipeControl : MonoBehaviour
     private SplinePoint[] points;
     private Dreamteck.Splines.SplineMesh splineMesh;
 
-    private float length;
 
     private void Awake()
     {
@@ -57,17 +56,15 @@ public class SwipeControl : MonoBehaviour
         {
             Ray _ray = m_camera.ScreenPointToRay(Input.touchCount == 1 ? (Vector3)Input.mousePosition : Input.mousePosition);
 
-
-
-
             if (Physics.Raycast(_ray, out hit, layer_mask))
             {
-                creatSplineObject(hit);
-                startDrawing(hit);
+                CreatSplineObject(hit);
+                StartDrawing(hit);
                 //PoolingSystem.Instance.InstantiateAPS("StarExplosion", hit.point);
-
-
+                
             }
+
+            m_currentRenderer = (GameObject)Instantiate(m_rendererPrefab, hit.point, Quaternion.identity, wingsPoint.transform);
         }
         else if (IsInput(TouchPhase.Moved) && LevelManager.Instance.IsLevelStarted && !isDrawComeFromOutside)
         {
@@ -77,111 +74,93 @@ public class SwipeControl : MonoBehaviour
 
             if (Physics.Raycast(_ray, out hit, layer_mask))
             {
-
-                if (Vector3.Distance(lastPos, hit.point) >= 1 && pointCount < points.Length)
+                
+                if (Vector3.Distance(lastPos, hit.point) >= 0.5f && pointCount < points.Length)
                 {
-                    drawWithMove(hit);
+                    DrawWithMove(hit);
                     //PoolingSystem.Instance.InstantiateAPS("StarExplosion", hit.point);
 
-
                 }
+
+                
+                
 
             }
         }
         else if (IsInput(TouchPhase.Ended))
         {
 
-            createdDrawObj.GetComponent<MeshRenderer>().enabled = true;
-
-            isDrawComeFromOutside = true;
-            clone = Instantiate(transformPanel, wingsPoint.transform.position, Quaternion.Euler(Vector3.zero));
-
-            clone.transform.position = wingsPoint.transform.position;
-            clone.transform.parent = wingsPoint.transform;
-            //createdDrawObj.transform.localPosition = new Vector3(createdDrawObj.transform.localPosition.x, createdDrawObj.transform.localPosition.y,0);
-            clone.transform.GetChild(0).transform.localPosition = new Vector3(createdDrawObj.transform.position.x, 0, 0);
-
-
-            pointCount = 0;
-            Destroy(m_currentRenderer);
-            Destroy(createdDrawObj);
+            EndOfDraw();
 
         }
     }
 
 
-    void creatSplineObject(RaycastHit hit)
+    void CreatSplineObject(RaycastHit hit)
     {
         startPos = hit.point;
         lastPos = startPos;
-
-
+        clone = null;
+        //Creation of base object for draw, Change layer for being sure it s not drawble on itself , Add Components
         createdDrawObj = (GameObject)Instantiate(drawObjPref, startPos, Quaternion.identity, transformPanel.transform);
         createdDrawObj.layer = LayerMask.NameToLayer("Ignore Raycast");
         createdDrawObj.GetComponent<MeshRenderer>().material = material;
         createdDrawObj.GetComponent<MeshRenderer>().enabled = false;
         spline = createdDrawObj.AddComponent<SplineComputer>();
 
-        //spline.uniformScale = false;
+        //SplineMesh Component adjust settings
         Dreamteck.Splines.SplineMesh splineMesh = createdDrawObj.AddComponent<Dreamteck.Splines.SplineMesh>();
         splineMesh.updateMethod = SplineUser.UpdateMethod.FixedUpdate;
         spline.updateMode = SplineComputer.UpdateMode.FixedUpdate;
-
-
         splineMesh.AddChannel(Wallmesh, "Wall");
         splineMesh.GetChannel(0).type = Dreamteck.Splines.SplineMesh.Channel.Type.Extrude;
         splineMesh.GetChannel(0).count = 50;
-
-
         splineMesh.GetChannel(0).minScale = new Vector3(0.1f, 1f, 0.1f);
         splineMesh.GetChannel(0).maxScale = new Vector3(0.1f, 1f, 0.1f);
         points = new SplinePoint[50];
-
         spline.type = Spline.Type.BSpline;
         spline.sampleMode = SplineComputer.SampleMode.Uniform;
         spline.customNormalInterpolation = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
         spline.customValueInterpolation = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
+
     }
-    void startDrawing(RaycastHit hit)
+    void StartDrawing(RaycastHit hit)
     {
-        if (hit.collider == null)
-        {
-            isDrawComeFromOutside = true;
-            Debug.Log("Its outside");
-        }
-        else
-        {
-            isDrawComeFromOutside = false;
-            points[pointCount] = new SplinePoint();
-            points[pointCount].position = hit.point;
-            points[pointCount].normal = Vector3.up;
-            points[pointCount].size = 1f;
-            points[pointCount].color = Color.white;
+       //Set drawComef.  ,  Placing first draw point and set it  , increase pointCount
+        isDrawComeFromOutside = false;
+        points[pointCount] = new SplinePoint();
+        points[pointCount].position = hit.point;
+        points[pointCount].normal = Vector3.up;
+        points[pointCount].size = 1f;
+        points[pointCount].color = Color.white;
 
-            spline.SetPoint(pointCount, points[pointCount], SplineComputer.Space.World);
-            lastPos = hit.point;
-            pointCount++;
+        spline.SetPoint(pointCount, points[pointCount], SplineComputer.Space.World);
+        lastPos = hit.point;
+        pointCount++;
 
-        }
+        
         isDrawComeFromOutside = false;
 
+        //Make disable all children of wingsPoint
         Transform[] wingsList = wingsPoint.GetComponentsInChildren<Transform>();
 
         for (int i = 0; i < wingsList.Length; i++)
         {
             wingsList[i].gameObject.SetActive(false);
+            //Or Destroy
         }
+        //Control for wingsPoint is enable
         wingsPoint.SetActive(true);
-        m_origin = hit.point;
-        m_currentRenderer = (GameObject)Instantiate(m_rendererPrefab, m_origin, Quaternion.identity, wingsPoint.transform);
-        m_currentRenderer.layer = 12;
+
+
+        
     }
-    void drawWithMove(RaycastHit hit)
+    void DrawWithMove(RaycastHit hit)
     {
         if (!isDrawComeFromOutside)
         {
-            Vector3 temp = lastPos;
-            // Debug.Log(pointCount);
+            
+            //Placing 3D draw points and mesh object creation
             points[pointCount] = new SplinePoint();
             points[pointCount].position = hit.point;
             points[pointCount].normal = Vector3.up;
@@ -191,16 +170,33 @@ public class SwipeControl : MonoBehaviour
             spline.SetPoint(pointCount, points[pointCount], SplineComputer.Space.World);
 
             lastPos = hit.point;
-            length += Vector3.Distance(temp, lastPos);
-            Debug.Log(length);
             pointCount++;
 
-
+            //2D draw with MouseMove
             m_currentRenderer.transform.position = hit.point;
+            m_currentRenderer.layer = 12;
         }
+    }
 
+    void EndOfDraw()
+    {
+        createdDrawObj.GetComponent<MeshRenderer>().enabled = true;
+        isDrawComeFromOutside = true;
 
+        //create copy of transformPanel and change position to on Fligt Wings
+        clone = Instantiate(transformPanel, wingsPoint.transform.position, Quaternion.Euler(Vector3.zero));
+        clone.transform.position = wingsPoint.transform.position;
+        clone.transform.parent = wingsPoint.transform;
 
+        clone.transform.GetChild(0).transform.localPosition = new Vector3(createdDrawObj.transform.position.x, 0, 0);
+        
+
+        
+        pointCount = 0;
+
+        //Destroy original draw obj. and 2d render object
+        Destroy(m_currentRenderer);
+        Destroy(createdDrawObj);
     }
     private bool IsInput(TouchPhase phase)
     {

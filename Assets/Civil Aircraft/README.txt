5232e128bc6e85f5cafe9a24e8769a7ec8414d4b
S4fuck <69677641+S4fuck@users.noreply.github.com>
2021-02-16 18:33:02 +0300

    Step 02

---
+++ Assets/Civil Aircraft/README.txt
@@ -0,0 +1,3 @@
+To control flaps, rotate them on local axis: Flaps - X axis, Rudder - Y axis, Control yoke - Z axis.
+
+Also keep in mind that I'm not a coder and scripts in demo folder are basic and for demo purposes only.
---
+++ Assets/propellarController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class propellarController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.AngleAxis(20, Vector3.forward);
+    }
+}
